53a84a55738604fb16ac0b154d9fb2f8f03bd38c
Alihan Musaev <143664018+oitarho@users.noreply.github.com>
2026-08-24 09:28:11 +0300

    Correct CI lint directive

--- scripts/doctor.sh
+++ scripts/doctor.sh
@@ -4,7 +4,8 @@ cd "${CICIG_DIR:-/opt/cicig}"
 docker compose config --quiet
 docker compose ps
 printf '\nDNS:\n'
-# shellcheck disable=SC1091 -- runtime configuration is created by install.sh
+# Runtime configuration is created by install.sh.
+# shellcheck disable=SC1091
 . ./.env
 getent ahostsv4 "$WG_DOMAIN" | awk '$2 == "STREAM" {print "  WG  " $1; exit}'
 getent ahostsv4 "$AWG_DOMAIN" | awk '$2 == "STREAM" {print "  AWG " $1; exit}'
